3dd35d12751e4c3fc7d95ef10a58dd6645248c5f
roberto_nzo <robertonzohabonayo@gmail.com>
2022-08-18 22:20:03 +0300

    remove all

--- interndashboard/main/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-main = Blueprint('main', __name__, template_folder='templates')
-
-@main.route('/')
-def home():
-    return render_template('home.html')
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from interndashboard import create_app
-
-app = create_app()
-app.secret_key = os.environ.get('secret_key')
-
-if __name__ == '__main__':
-    app.run(debug=True)
